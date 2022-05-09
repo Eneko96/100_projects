acf0d17e9d894cabab6336ba8564cc910e4a7832
Eneko96 <imr.aem2526@gmail.com>
2022-05-09 19:49:13 +0200

    added first project (password generator)

---
+++ pswrd_generator/src/main.ts
@@ -0,0 +1,44 @@
+import './style.css'
+
+const formNames = ({
+  lengthName: 'lengthPwd',
+  include_numbers: 'include-numbers',
+  include_symbols: 'include-symbols',
+})
+
+const charset = (inNum: boolean, inSym: boolean) => {
+  const baseCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let res = baseCharset
+  if (inNum) res = baseCharset + "0123456789"
+  if (inSym) res += "!@#$%^&*()_+"
+  return res
+}
+
+// generate password
+function generatePassword(len = 5, inNum = false, inSym = false) {
+  var length = len
+  let charSet = charset(inNum, inSym)
+  let retVal = ''
+  for (var i = 0, n = charSet.length; i < length; ++i) {
+    retVal += charSet.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+
+const $ = (selector: any) => document.querySelector(selector);
+
+const $generate = $('#generator') as HTMLFormElement
+const $result = $('#result') as HTMLDivElement
+
+$generate.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const form = event.target
+  const length = form?.elements[formNames.lengthName]?.value
+  const includeNumbers = form?.elements[formNames.include_numbers]?.checked
+  const includeSymbols = form?.elements[formNames.include_symbols]?.checked
+  console.log(length, includeNumbers, includeSymbols)
+  const password = generatePassword(length, includeNumbers, includeSymbols)
+  $result.firstChild.textContent = password
+})
+
